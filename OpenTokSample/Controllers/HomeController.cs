69e42e3467a2d69444f82147a510461c5ea83b9b
Ankur Oberoi <aoberoi@gmail.com>
2012-09-14 15:30:01 -0400

    adding project files

---
+++ OpenTokSample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenTok;
+
+namespace OpenTokSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+            ViewBag.APIKey = appSettings["opentok_key"];
+
+            OpenTokSDK opentok = new OpenTokSDK();
+
+            string sessionId = opentok.CreateSession(Request.ServerVariables["REMOTE_ADDR"]);
+
+            Dictionary<string, object> tokenOptions = new Dictionary<string, object>();
+            tokenOptions.Add(TokenPropertyConstants.ROLE, RoleConstants.MODERATOR);
+            ViewBag.Token = opentok.GenerateToken(sessionId, tokenOptions);
+            return View();
+        }
+
+    }
+}
---
+++ OpenTokSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenTokSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenTokSample")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2794f2cf-805f-46db-9c76-b40a48be7c4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
